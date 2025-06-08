ed3d5eb2c4ce941f65cb33a2ef43afcbd60c6b31
Boniface Mwandobo <breezojr@gmail.com>
2025-06-08 18:11:55 +0300

    login page refactor for cleaning make sure only required codes

---
+++ lib/features/auth/models/user_model.dart
@@ -0,0 +1,32 @@
+class UserModel {
+  final int id;
+  final String name;
+  final String email;
+  final String token;
+
+  UserModel({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.token,
+  });
+
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+
+    return UserModel(
+      id: json['id'],
+      name: json['full_name'],
+      email: json['email'],
+      token: json['token'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'email': email,
+      'token': token,
+    };
+  }
+}
--- lib/features/auth/service/auth_service.dart
+++ lib/features/auth/service/auth_service.dart
@@ -1,22 +1,32 @@
+import 'dart:convert';
 
 import 'package:flex_mobile/core/api/api_constants.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../../../core/api/api_service.dart';
+import '../models/user_model.dart';
 
 class AuthService {
-
   Future<(bool, String)> login(String email, String password) async {
-    final response = await ApiService().request(ApiConstants.post,ApiConstants.loginEndpoint, data: {
+    final response = await ApiService()
+        .request(ApiConstants.post, ApiConstants.loginEndpoint, data: {
       'email': email,
       'password': password,
     });
 
-    final token = response['user']['token'] as String;
-    final id = response['user']['id'];
+    final userJson = response['user'];
+    final user = UserModel.fromJson(userJson);
+
     final prefs = await SharedPreferences.getInstance();
-    await prefs.setInt('userId', id);
+    await prefs.setString('user', jsonEncode(user.toJson()));
     await prefs.setBool('isLoggedIn', true);
-    ApiService().updateToken(token);
+    ApiService().updateToken(user.token);
+
+    // final token = response['user']['token'] as String;
+    // final id = response['user']['id'];
+    // final prefs = await SharedPreferences.getInstance();
+    // await prefs.setInt('userId', id);
+    // await prefs.setBool('isLoggedIn', true);
+    // ApiService().updateToken(token);
     return (true, 'Login successful');
   }
 
